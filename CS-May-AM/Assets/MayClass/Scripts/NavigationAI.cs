8d500c2da89429f7b5483d8b117ad9e628c38264
cyb3r6 <45832126+cyb3r6@users.noreply.github.com>
2019-07-17 12:14:04 -0600

    added navigation AI and finished paintbrush stuff

---
+++ CS-May-AM/Assets/MayClass/Scripts/NavigationAI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationAI : MonoBehaviour
+{
+    public Transform chasee;
+    private NavMeshAgent ghostAgent;
+
+    void Start()
+    {
+        ghostAgent = GetComponent<NavMeshAgent>();
+    }
+    
+    void Update()
+    {
+        ghostAgent.SetDestination(chasee.position);
+    }
+}
---
+++ CS-May-AM/Assets/MayClass/Scripts/PaintBrush.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintBrush : MonoBehaviour
+{
+    private Material paint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Paint")
+        {
+            paint = other.GetComponent<Renderer>().material;
+            this.GetComponent<Renderer>().material = paint;
+        }
+        else if(other.tag == "Paintable")
+        {
+            other.GetComponent<Renderer>().material = paint;
+        }
+    }
+}
